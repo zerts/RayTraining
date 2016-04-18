61b4007001d33684b9977d164c8d54b3bf038a90
Andrey <zertsalov96@yandex.ru>
2016-04-18 14:33:12 +0300

    BoundinBox added

--- RayTraining/RayTraining/BoundinBox.h
+++ RayTraining/RayTraining/BoundinBox.h
@@ -8,11 +8,18 @@ using namespace std;
 
 class BoundinBox {
 private:
-	MyPoint xu, xd, yu, yd, zu, zd;
+	long double xu, xd, yu, yd, zu, zd;
 
 public:
 	BoundinBox() {};
 	BoundinBox(MyPoint first, MyPoint second) {
+		xu = max(first.getX(), second.getX());
+		xd = min(first.getX(), second.getX());
 
+		yu = max(first.getY(), second.getY());
+		yd = min(first.getY(), second.getY());
+
+		zu = max(first.getZ(), second.getZ());
+		zd = min(first.getZ(), second.getZ());
 	}
 };
